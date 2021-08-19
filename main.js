5fe813112ab82f9f190d6ff47b370ec3fcb8ae3d
hithere1012 <hithere1012@naver.com>
2021-08-19 11:00:00 +0900

    pose predict function using teachablemachine pose model

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ app.use(express.json({ limit : "50mb" }));
 
 const URL = "https://teachablemachine.withgoogle.com/models/ZXEZyolyru/";
 let model, maxPredictions;
+
+async function predict(img_base64){
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+    model = await tmPose.load(modelURL, metadataURL);
+
+    tmPose.load(modelURL, metadataURL).then(mod => {
+        model = mod;
+        maxPredictions = model.getTotalClasses();
+
+    }).catch(err => console.log(err));
+
+    const buffer = Buffer.from(img_base64, "base64");
+    const img = await Canvas.loadImage(buffer);
+    var new_canvas = new createCanvas(img.width, img.height);
+    var context = new_canvas.getContext('2d');
+    img.onload = function (){
+        context.drawImage(img, 0, 0, img.width, img.height);
+    }
+
+    const { pose, posenetOutput } = await model.estimatePose(new_canvas, false);
+    const prediction = await model.predict(posenetOutput);
+    return new Promise(resolve => {
+        setTimeout(() => resolve(prediction));
+    });
+}
