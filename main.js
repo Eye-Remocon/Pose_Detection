6a263d4712b9d4020c87b328faca8a5d90fcea14
hithere1012 <hithere1012@naver.com>
2021-08-22 22:47:50 +0900

    Code Refactoring in predict function

--- main.js
+++ main.js
@@ -21,26 +21,12 @@ async function init(){
 }
 
 async function predict(img_base64){
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
-    model = await tmPose.load(modelURL, metadataURL);
-
-    tmPose.load(modelURL, metadataURL).then(mod => {
-        model = mod;
-        maxPredictions = model.getTotalClasses();
-
-    }).catch(err => console.log(err));
-
     const buffer = Buffer.from(img_base64, "base64");
     const img = await Canvas.loadImage(buffer);
-    var new_canvas = new createCanvas(img.width, img.height);
-    var context = new_canvas.getContext('2d');
-    img.onload = function (){
-        context.drawImage(img, 0, 0, img.width, img.height);
-    }
+    context.drawImage(img, 0, 0);
 
     const { pose, posenetOutput } = await model.estimatePose(new_canvas, false);
-    const prediction = await model.predict(posenetOutput);
+    const prediction = await model.predictTopK(posenetOutput, 3);
     return new Promise(resolve => {
         setTimeout(() => resolve(prediction));
     });
